34d455913be3eef4732845d46a386750a1ca108f
Lusficer <bachhuy410@gmail.com>
2023-09-21 11:49:08 +0700

    Update index.js

--- Backend/config/db/index.js
+++ Backend/config/db/index.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Discord_Bot", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://Hoang145:Anhongdo20@cluster0.lwrnk8h.mongodb.net/DiscordBot",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("connnect successful");
   } catch (error) {
     console.log("connect failed");
